6215f3ce7e57a6b3cbeed47de16890ce64113c2b
janar363 <janardhankarravula363@gmail.com>
2023-10-27 09:36:28 -0500

    added inital tests

--- test_backend.py
+++ test_backend.py
@@ -27,3 +27,35 @@ def test_signup(test_client):
         db.session.delete(registered_user)
 
         db.session.commit()
+
+
+def test_login(test_client):
+    form_data = {'email': 'test', 'password': 'test'}
+
+    response = test_client.post('/login', data=form_data)
+
+    with test_client.session_transaction() as session:
+        assert session['loggedIn'] == True 
+
+def test_update_password(test_client):
+    form_data = {'password': 'testnew', 'confirmPassword': 'testnew'}
+    forgot_email = 'test'
+    old_password = 'test'
+
+    with test_client.session_transaction() as session:
+        session['forgot email'] = forgot_email
+
+    response = test_client.post('/update_password', data=form_data)
+
+    with app.app_context():
+        registered_user = db.session.get(User, forgot_email)
+        assert registered_user.password == form_data['password']
+
+        registered_user.password = old_password
+
+        db.session.commit()
+
+
+
+
+
